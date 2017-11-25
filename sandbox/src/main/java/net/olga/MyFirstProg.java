2629999e89d7910ee9c230b952486ca4006d29e1
Olga <olga.kuratkina@gmail.com>
2017-11-25 17:59:30 +0200

    added more functions to sandbox

--- sandbox/src/main/java/net/olga/MyFirstProg.java
+++ sandbox/src/main/java/net/olga/MyFirstProg.java
@@ -3,6 +3,19 @@ package net.olga;
 public class MyFirstProg {
 
 	public static void main(String[] Args) {
-		System.out.println("Hello, World!");
-	}
+		hello("Olga");
+		hello("World");
+        System.out.println(area(6));
+        System.out.println(area(5,7));
+    }
+
+    private static void hello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+    private static double area(double a, double b) {
+	    return a * b;
+    }
+    private static double area(double a) {
+        return a * a;
+    }
 }
