dad0797eed73f00e43b23c4c3d4954920cea2e4c
Olga <olga.kuratkina@gmail.com>
2017-12-09 21:04:40 +0200

    contact creation common method

--- addressbook-web-tests/src/test/java/net/olga/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/net/olga/addressbook/tests/ContactCreationTests.java
@@ -8,10 +8,8 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getNavipHelper().gotoHomePage();
-        app.getContactpHelper().initContactCreation();
-        app.getContactpHelper().fillContactForm(new ContactData("Miguel", "Alberto", "Navarro", "Mig",
+        app.getContactpHelper().createContact(new ContactData("Miguel", "Alberto", "Navarro", "Mig",
                 "Mr", "Madrid Spain", "3409867784532", "mig@navarro.io"));
-        app.getContactpHelper().submitContactCreation();
         app.getNavipHelper().gotoHomePage();
     }
 }
