c4bc8c2c8ff94bdf2433dd4a505b7a17809cfaea
Olga <olga.kuratkina@gmail.com>
2017-12-17 15:16:45 +0200

    added parameter index

--- addressbook-web-tests/src/test/java/net/olga/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/net/olga/addressbook/tests/GroupDeletionTests.java
@@ -10,17 +10,18 @@ public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
+        int index = 0;
         app.getNavipHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(0);
+        app.getGroupHelper().selectGroup(index);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(), after.size() + 1);
-        before.remove(0);
+        before.remove(index);
         Assert.assertEquals(before, after);
     }
 
