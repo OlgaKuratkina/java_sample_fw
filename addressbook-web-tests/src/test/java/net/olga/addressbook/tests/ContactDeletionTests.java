af25062daadfde702a15d019315a0ea4ce0a2391
Olga <olga.kuratkina@gmail.com>
2017-12-17 15:21:39 +0200

    improved assertion in contact deletion test

--- addressbook-web-tests/src/test/java/net/olga/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/net/olga/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,16 @@
 package net.olga.addressbook.tests;
 
 import net.olga.addressbook.models.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
     @Test
     public void testContactDeletion() {
+        int index = 0;
         app.getNavipHelper().gotoHomePage();
         if (! app.getContactpHelper().isThereContact()) {
             app.getContactpHelper().createContact(new ContactData("Miguel", "Alberto",
@@ -14,9 +18,15 @@ public class ContactDeletionTests extends TestBase {
                     "3409867784532", "mig@navarro.io"));
         }
         app.getNavipHelper().gotoHomePage();
-        app.getContactpHelper().selectContact();
+        List<ContactData> before = app.getContactpHelper().getContactList();
+        app.getContactpHelper().selectContact(index);
         app.getContactpHelper().deleteSelectedContacts();
         app.getContactpHelper().acceptAlert();
         app.getNavipHelper().gotoHomePage();
+        List<ContactData> after = app.getContactpHelper().getContactList();
+        Assert.assertEquals(before.size(), after.size() + 1);
+
+        before.remove(index);
+        Assert.assertEquals(before, after);
     }
 }
