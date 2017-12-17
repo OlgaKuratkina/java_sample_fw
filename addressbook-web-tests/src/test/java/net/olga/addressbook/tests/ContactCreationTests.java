35a9985d4396078cfb274e73fdfc182318c9fbb9
Olga <olga.kuratkina@gmail.com>
2017-12-17 15:22:45 +0200

    improved assertion in contact creation test

--- addressbook-web-tests/src/test/java/net/olga/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/net/olga/addressbook/tests/ContactCreationTests.java
@@ -1,15 +1,30 @@
 package net.olga.addressbook.tests;
 
 import net.olga.addressbook.models.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
         app.getNavipHelper().gotoHomePage();
-        app.getContactpHelper().createContact(new ContactData("Miguel", "Alberto", "Navarro", "Mig",
-                "Mr", "Madrid Spain", "3409867784532", "mig@navarro.io"));
+        List<ContactData> before = app.getContactpHelper().getContactList();
+        ContactData new_contact = new ContactData("Miguel", "Alberto", "Navarro", "Mig",
+                "Mr", "Madrid Spain", "3409867784532", "mig@navarro.io");
+        app.getContactpHelper().createContact(new_contact);
         app.getNavipHelper().gotoHomePage();
+        List<ContactData> after = app.getContactpHelper().getContactList();
+        before.add(new_contact);
+        Assert.assertEquals(before.size(), after.size());
+
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
     }
 }
