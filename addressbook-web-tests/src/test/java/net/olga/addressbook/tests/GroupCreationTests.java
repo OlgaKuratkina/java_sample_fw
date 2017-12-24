5d09adc8a34a2f9d95a3a4e17b82283dbe7c8c1f
Olga <olga.kuratkina@gmail.com>
2017-12-24 18:58:01 +0200

    edited creation groups test for set instead of list

--- addressbook-web-tests/src/test/java/net/olga/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/net/olga/addressbook/tests/GroupCreationTests.java
@@ -6,23 +6,23 @@ import org.testng.annotations.Test;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreationDefault() {
         app.goTo().groupPage();
-        List<GroupData> before = app.group().list();
+        Set<GroupData> before = app.group().all();
         GroupData group = new GroupData().withName("test2");
         app.group().create(group);
-        List<GroupData> after = app.group().list();
+        Set<GroupData> after = app.group().all();
 
+        Assert.assertEquals(before.size(), after.size()-1);
+
+//        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
         before.add(group);
-        Assert.assertEquals(before.size(), after.size());
-
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
         Assert.assertEquals(before, after);
     }
 
